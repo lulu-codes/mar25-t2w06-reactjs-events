98e0393c8cd663721decdee3aeab55b805ece97f
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-14 17:08:30 +0800

    feat: PokemonSearcher started, PokemonDataDisplayer created

---
+++ src/components/PokemonSearcher.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+
+export class PokemonSearcher extends React.Component {
+	constructor(){
+		super();
+
+		// data that this component must track
+		this.state = {
+			pokemonToSearch: "pikachu",
+			responseName: "",
+			responseSpriteUrl: "",
+			// Saving the whole API response to an object is fine
+			// for prototyping, but provides heaps of room for mistakes as the project grows
+			// so try to only track specific properties from the API!
+			// response: {}
+		}
+	}
+
+	// have an event for handling form submission
+
+
+	render(){
+		return <section className="pokemonSearcherContainer">
+			{/*
+				<PokemonSearcherForm
+					currentSearchFieldValue={pokemonToSearch}
+					searchValueUpdateFunction={() => {this.setState({})}}
+					formSubmissionHandler={() => {}}
+
+				/>
+			*/}
+			{/*
+			 	<PokemonSearcherDataDisplay
+					nameToShow={this.state.responseName}
+					imageToShow={this.state.responseSpriteUrl}
+				/>
+			*/}
+		</section>
+	}
+}
---
+++ src/components/PokemonSearcherDataDisplay.jsx
@@ -0,0 +1,20 @@
+
+
+/*
+<PokemonSearcherDataDisplay
+	nameToShow={this.state.responseName}
+	imageToShow={this.state.responseSpriteUrl}
+/>
+*/
+
+export function PokemonSearcherDataDisplay({nameToShow, imageToShow}){
+
+	if (nameToShow == "" || imageToShow == "") {
+		return null;
+	} else {
+		return <section className="pokemonDataDisplay">
+			<h1>{nameToShow}</h1>
+			<img src={imageToShow} alt={`Sprite of the Pokemon ${nameToShow}`} />
+		</section>
+	}
+}
