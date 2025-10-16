32495bc54a2db6974b6d96fb070fcbf65b1fb4c1
Nhi Huynh <nhi.huynh@hotmail.com>
2025-10-16 17:49:36 +0800

    feat!: actual routing implemented!

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Homepage } from './pages/Homepage.jsx'
+import { SearchPage } from './pages/SearchPage.jsx'
+import { AboutPage } from './pages/AboutPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/search">Search</a>
+
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/search" element={<SearchPage />} />
+
+      </Routes>
+
+      {/* <App /> */}
     </BrowserRouter>
 
   </StrictMode>,
